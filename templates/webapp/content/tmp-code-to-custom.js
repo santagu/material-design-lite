6389bef05e60f04957c559704574910025023356
salvador.santander <salvador.santander@gmail.com>
2016-07-25 07:48:32 -0400

    terminada parte estatica version 1

--- templates/webapp/content/tmp-code-to-custom.js
+++ templates/webapp/content/tmp-code-to-custom.js
@@ -42,29 +42,50 @@
     });
   }
 
+  function addOnClickShowModalDialog(elementToAddEvent, dialogToShow) {
+   elementToAddEvent.addEventListener('click', function() {
+      // Registramos el polyfill por si el navegador no soporta elemento dialog
+      if (!dialogToShow.showModal) {
+        dialogPolyfill.registerDialog(dialogToShow);
+      }
+      dialogToShow.showModal();
+    }); 
+  }
+  
   /**
    * Ejecuta todo el js necesario para que funcione el diálogo de búsqueda
    * avanzada cuando se pulsa sobre el icono de la barra de herramientas
    * o la ayuda de la búsqueda
    */
   function initializeExtendedDialogActions() {
+    // No cogemos los tr porque tienen un tratamiento diferente
     var elementsWichOpenDialog =
-      document.querySelectorAll("*[data-mdl-on-click-open-dialog-id]");
+      document.querySelectorAll("*:not(tr)[data-mdl-on-click-open-dialog-id]");
 
     // Por cada nodo se añade evento para que al pulsar se abra el dialogo
     forEachNode(elementsWichOpenDialog, function(index, value) {
       var dialogIdToShow = value.getAttribute("data-mdl-on-click-open-dialog-id");
       var dialogToShow = document.querySelector("#" + dialogIdToShow);
-      value.addEventListener('click', function() {
-        // Registramos el polyfill por si el navegador no soporta elemento dialog
-        if (!dialogToShow.showModal) {
-          dialogPolyfill.registerDialog(dialogToShow);
-        }
-        dialogToShow.showModal();
+      
+      addOnClickShowModalDialog(value, dialogToShow);
+    });
+    
+    // Los tr los tratamos diferentes, porque tenemos que coger sus 
+    var tableRowsWichOpenDialog =
+      document.querySelectorAll("tr[data-mdl-on-click-open-dialog-id]");
+
+    // Por cada nodo se añade evento para que al pulsar se abra el dialogo
+    forEachNode(tableRowsWichOpenDialog, function(index, value) {
+      var dialogIdToShow = value.getAttribute("data-mdl-on-click-open-dialog-id");
+      var dialogToShow = document.querySelector("#" + dialogIdToShow);
+      var tableCells = value.querySelectorAll("td:not(.mdl-data-table__cell--actions)");
+
+      forEachNode(tableCells, function(index, value) {
+        addOnClickShowModalDialog(value, dialogToShow);
       });
     });    
       
-     var elementsWichCloseDialog =
+    var elementsWichCloseDialog =
       document.querySelectorAll("*[data-mdl-on-click-close-dialog-id]");
 
     // Por cada nodo se añade evento para que al pulsar se abra el dialogo
@@ -82,6 +103,7 @@
     
   }
 
+  
   /**
    * Para recorrer un NodeList tal como se haría con un array y foreach
    * https://css-tricks.com/snippets/javascript/loop-queryselectorall-matches
